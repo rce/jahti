dee486abaf5ab4ab001bc0c7efdc3d8bc3d8f073
Henry Heikkinen <rce@rce.fi>
2015-05-15 23:26:45 +0300

    Read the character table from command line.

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,20 @@
 module Main where
 
+import System.Environment (getArgs)
 import Trie (Trie, mkTrie, contains)
 import Jahti (Table, generateWords)
 
-table :: Table
-table =
-  [ "ielh"
-  , "jvti"
-  , "aäun"
-  , "hkio"
-  ]
-
 main :: IO ()
 main = do
+  args <- getArgs
+  let table = parseTable (args !! 0)
   content <- readFile "words.txt"
   let trie = mkTrie (lines content)
   mapM_ putStrLn $ findRealWords table trie
+
+parseTable :: String -> Table
+parseTable [] = []
+parseTable cs = [(take 4 cs)] ++ parseTable (drop 4 cs)
 
 findRealWords :: Table -> Trie -> [String]
 findRealWords table wordTrie = filter isValid $ generateWords table
